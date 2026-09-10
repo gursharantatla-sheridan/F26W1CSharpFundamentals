dd90baef817b303164c9ef75fe6a67571759e738
gursharantatla-sheridan <sing1731@shernet.sheridancollege.ca>
2026-09-10 13:54:59 -0400

    vars

--- F26W1CSharpFundamentals/Program.cs
+++ F26W1CSharpFundamentals/Program.cs
@@ -2,9 +2,29 @@
 {
     internal class Program
     {
+        //static int i;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int @double = 5;
+
+            double d = 5.5;
+            float f = 5.5F;
+            decimal m = 5.5M;
+
+            int i = 5;
+            long l = 5L;
+
+            string s = "hello";
+
+            int z = i + 10;
+            //Console.WriteLine(z);
+
+            var x = 5;
+            //x = "hello";
+
+
+
         }
     }
 }
