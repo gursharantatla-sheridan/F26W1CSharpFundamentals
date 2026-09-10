4cd52d08cf8f770987d481c4db9c9cef7f823bdc
gursharantatla-sheridan <sing1731@shernet.sheridancollege.ca>
2026-09-10 15:11:54 -0400

    ref types

--- F26W1CSharpFundamentals/Program.cs
+++ F26W1CSharpFundamentals/Program.cs
@@ -15,6 +15,8 @@
             int i = 5;
             long l = 5L;
 
+            i = (int)d;
+
             string s = "hello";
 
             int z = i + 10;
@@ -34,6 +36,65 @@
 
             // string interpolation
             Console.WriteLine($"I have {cars} cars, {bikes} bikes and {trucks} trucks");
+
+
+            Console.WriteLine("\n\n\n");
+
+
+
+
+            // value type and reference type variables
+
+            int a = 5;
+            int b = a;
+
+            a++;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+
+            b = b + 100;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            Console.WriteLine("\n\n\n");
+
+
+
+            Circle c1 = new Circle();
+            Console.WriteLine(c1.radius);
+
+            c1.radius = 50;
+            Console.WriteLine(c1.radius);
+
+            Circle c2 = c1;
+            Console.WriteLine(c2.radius);
+
+            c2.radius = 100;
+            Console.WriteLine(c1.radius);
+            Console.WriteLine(c2.radius);
+
+            Console.WriteLine("\n\n\n");
+
+
+
+            string s1 = "hello";
+            string s2 = s1;
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+
+            s2 = "bye";
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
         }
+    }
+
+    class Circle
+    {
+        public int radius;
     }
 }
