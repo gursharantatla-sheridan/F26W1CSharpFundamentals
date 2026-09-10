8aacab214fd00274ecdc019c28bbabe19d970ef7
gursharantatla-sheridan <sing1731@shernet.sheridancollege.ca>
2026-09-10 13:20:10 -0400

    Add project files.

---
+++ F26W1CSharpFundamentals/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace F26W1CSharpFundamentals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
